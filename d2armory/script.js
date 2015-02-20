78eea557af493f7ac2fd3dcdd4ed8001963d6a65
unknown <bnguyen@NINTL-2F3KWZ1.HQ.Nerium.com>
2015-02-20 14:17:41 -0600

    Added the API call and it works successfuly, removed CORs from IE Still need headers

--- d2armory/script.js
+++ d2armory/script.js
@@ -5,3 +5,28 @@ angular
     .controller("MainController", MainController);
 
 MainController.$inject = ['$scope', '$http'];
+
+function MainController($scope, $http) {
+    $scope.battleTag = "Billy-1739";
+
+
+    var onUserComplete = function (response) {
+        $scope.account = response.data;
+
+    };
+
+    var onRepos = function (reponse) {
+        $scope.repos = response.data;
+    }
+
+    var onError = function () {
+        $scope.error = "No results found."
+    };
+
+    $scope.search = function (battleTag) {
+        $http.get("http://us.battle.net/api/d3/profile/" + battleTag + "/")
+            .then(onUserComplete, onError);
+    };
+
+
+}
