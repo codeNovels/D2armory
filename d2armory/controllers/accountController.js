aff23642a58eb4edb8c49e021be1842f1c538d86
Billy Nguyen <codeNovels@gmail.com>
2015-02-23 14:53:44 -0600

    Pulled in Blizzard HTML and CSS. Still need to wire up

---
+++ d2armory/controllers/accountController.js
@@ -0,0 +1,48 @@
+﻿var app = angular.module("d2armory", []);
+
+angular
+    .module("d2armory")
+    .controller("accountController", accountController);
+
+accountController.$inject = ['$scope', '$http'];
+
+function accountController($scope, $http) {
+    //$scope.battleTag = "Billy-1739";
+    //$scope.heroID = 0;
+
+    var onUserComplete = function (response) {
+        $scope.account = response.data;
+    };
+    
+    var onUserSelect = function (response) {
+        $scope.heroDetails = response.data;
+    };
+
+    //var onRepos = function (reponse) {
+    //    $scope.repos = response.data;
+    //};
+
+    var onError = function () {
+        $scope.error = "No results found."
+    };
+
+    $scope.search = function (battleTag) {
+        $http.get("http://us.battle.net/api/d3/profile/" + battleTag + "/")
+            .then(onUserComplete, onError);
+    };
+
+    $scope.chooseHero = function (battleTag, heroID) {
+        $http.get("http://us.battle.net/api/d3/profile/" + battleTag + "/hero/" + heroID)
+            .then(onUserSelect, onError);
+    };
+
+
+
+
+
+
+
+
+
+
+}
