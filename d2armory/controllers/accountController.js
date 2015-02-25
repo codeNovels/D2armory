56c5ab655131416b3fb70b57f6ed63cde784caa6
Aaron Bickle <abickle@nerium.com>
2015-02-24 23:33:52 -0600

    Refactored item details request logic to use less code by adding a slot list.
    Slot list can also be used in view to reduce written HTML footprint.

--- d2armory/controllers/accountController.js
+++ d2armory/controllers/accountController.js
@@ -2,12 +2,19 @@
 
 angular
     .module("d2armory")
+    .constant('apiUrl', 'http://us.battle.net/api/d3/')
     .controller("accountController", accountController);
 
-accountController.$inject = ['$scope', '$http'];
+accountController.$inject = ['$scope', '$http', 'apiUrl'];
 
-function accountController($scope, $http) {
+function accountController($scope, $http, apiUrl) {
     $scope.battleTag = "Billy-1739";
+
+    $scope.slots = ['head', 'torso', 'feet', 'hands', 'shoulders', 'legs', 'bracers', 'mainHand', 'offHand', 'waist', 'rightFinger', 'leftFinger', 'neck'];
+
+    $scope.items = {};
+
+    var slotMap = { 'shoulder': 'shoulders', 'chest': 'torso' }; // object that identifies slot discrepancies between hero details and item details
 
     //ACCOUNT
     var onUserComplete = function (response) {
@@ -18,66 +25,9 @@ function accountController($scope, $http) {
         $scope.heroDetails = heroDetails.data;
     };
 
-
-
-    //ITEMS
-    var onHeadComplete = function (headDetails) {
-        $scope.headDetails = headDetails.data;   
-    };
-    var onTorsoComplete = function (torsoDetails){
-        $scope.torsoDetails = torsoDetails.data;
-    };
-
-    var onFeetComplete = function (feetDetails){
-        $scope.feetDetails = feetDetails.data;
-    };
-
-    var onHandsComplete = function(handsDetails) {
-        $scope.handsDetails = handsDetails.data;
-    };
-
-    var onShouldersComplete = function(shouldersDetails) {
-        $scope.shouldersDetails = shouldersDetails.data;
-    };
-
-    var onLegsComplete = function(legsDetails){
-        $scope.legsDetails = legsDetails.data;
-    };
-
-    var onBracersComplete = function(bracersDetails){
-        $scope.bracersDetails = bracersDetails.data;
-    };
-
-    var onMainHandComplete = function(mainHandDetails){
-        $scope.mainHandDetails = mainHandDetails.data;
-    };
-
-    var onOffHandComplete = function(offHandDetails){
-        $scope.offHandDetails = offHandDetails.data;
-    };
-
-    var onWaistComplete = function(waistDetails){
-        $scope.waistDetails = waistDetails.data;
-    };
-
-    var onRightFingerComplete = function(rightFingerDetails){
-        $scope.rightFingerDetails = rightFingerDetails.data;
-    };
-
-    var onLeftFingerComplete = function(leftFingerDetails){
-        $scope.leftFingerDetails = leftFingerDetails.data;
-    };
-
-    var onNeckComplete = function(neckDetails){
-        $scope.neckDetails = neckDetails.data;
-    };
-
     var onError = function () {
         $scope.error = "No results found."
     };
-
-
-
 
     $scope.search = function (battleTag) {
         $http.get("http://us.battle.net/api/d3/profile/" + battleTag + "/")
@@ -85,111 +35,47 @@ function accountController($scope, $http) {
     };
 
     $scope.chooseHero = function (battleTag, heroID) {
-        $http.get("http://us.battle.net/api/d3/profile/" + battleTag + "/hero/" + heroID)
+        $http.get(apiUrl + "profile/" + battleTag + "/hero/" + heroID)
             .then(onUserSelect, onError);
     };
 
-    $scope.headItemDetails = function (headItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + headItemID)
-            .then(onHeadComplete, onError);
-    };
+    var onItemDetailsComplete = function (itemDetails) {
+        var data = itemDetails.data; // redefined itemDetails to the data
+        var itemTooltip = data.tooltipParams;
+        var itemSlots = data.slots; // slots item can be put in
 
-    $scope.torsoItemDetails = function (torsoItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + torsoItemID)
-            .then(onTorsoComplete, onError);
-    };
+        angular.forEach(itemSlots, function (slot) {
+            if (slotMap[slot]) {
+                slot = slotMap[slot];
+            }
 
-    $scope.feetItemDetails = function (feetItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + feetItemID)
-            .then(onFeetComplete, onError);
-    };
+            if ($scope.heroDetails.items[slot] && $scope.heroDetails.items[slot].tooltipParams === itemTooltip) {
+                $scope.items[slot + 'Details'] = data;
+                return; // stop looping
+            }
+        });
+    }
 
-    $scope.handsItemDetails = function (handsItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + handsItemID)
-            .then(onHandsComplete, onError);
-    };
+    var getItemDetails = function (itemId) {
+        $http.get(apiUrl + "data/" + itemId)
+            .then(onItemDetailsComplete, onError);
+    }
 
-    $scope.shouldersItemDetails = function (shouldersItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + shouldersItemID)
-            .then(onShouldersComplete, onError);
-    };
-
-    $scope.legsItemDetails = function (legsItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + legsItemID)
-            .then(onLegsComplete, onError);
-    };
-
-    $scope.bracersItemDetails = function (bracersItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + bracersItemID)
-            .then(onBracersComplete, onError);
-    };
-
-    $scope.mainHandItemDetails = function (mainHandItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + mainHandItemID)
-            .then(onMainHandComplete, onError);
-    };
-
-    $scope.offHandItemDetails = function (offHandItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + offHandItemID)
-            .then(onOffHandComplete, onError);
-    };
-
-    $scope.waistItemDetails = function (waistItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + waistItemID)
-            .then(onWaistComplete, onError);
-    };
-
-    $scope.rightFingerItemDetails = function (rightFingerItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + rightFingerItemID)
-            .then(onRightFingerComplete, onError);
-    };
-
-    $scope.leftFingerItemDetails = function (leftFingerItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + leftFingerItemID)
-            .then(onLeftFingerComplete, onError);
-    };
-
-    $scope.neckItemDetails = function (neckItemID) {
-        $http.get("http://us.battle.net/api/d3/data/" + neckItemID)
-            .then(onNeckComplete, onError);
-    };
-
-    $scope.getDetails = function (battleTag, heroID, headItemID, torsoItemID, feetItemID, handsItemID, shouldersItemID, legsItemID, bracersItemID, mainHandItemID, offHandItemID, waistItemID, rightFingerItemID, leftFingerItemID, neckItemID) {
+    $scope.getDetails = function (battleTag, heroID) {
         $scope.chooseHero(battleTag, heroID);
-        $scope.headItemDetails(headItemID);
-        $scope.torsoItemDetails(torsoItemID);
-        $scope.feetItemDetails(feetItemID);
-        $scope.handsItemDetails(handsItemID);
-        $scope.shouldersItemDetails(shouldersItemID);
-        $scope.legsItemDetails(legsItemID);
-        $scope.bracersItemDetails(bracersItemID);
-        $scope.mainHandItemDetails(mainHandItemID);
-        $scope.offHandItemDetails(offHandItemID);
-        $scope.waistItemDetails(waistItemID);
-        $scope.rightFingerItemDetails(rightFingerItemID);
-        $scope.leftFingerItemDetails(leftFingerItemID);
-        $scope.neckItemDetails(neckItemID);
     };
 
     $scope.$watch("heroDetails", function (newValue, oldValue) {
         if (newValue) {
-            $scope.headItemDetails($scope.heroDetails.items.head.tooltipParams);
-            $scope.torsoItemDetails($scope.heroDetails.items.torso.tooltipParams);
-            $scope.feetItemDetails($scope.heroDetails.items.feet.tooltipParams);
-            $scope.handsItemDetails($scope.heroDetails.items.hands.tooltipParams);
-            $scope.shouldersItemDetails($scope.heroDetails.items.shoulders.tooltipParams);
-            $scope.legsItemDetails($scope.heroDetails.items.legs.tooltipParams);
-            $scope.bracersItemDetails($scope.heroDetails.items.bracers.tooltipParams);
-            $scope.mainHandItemDetails($scope.heroDetails.items.mainHand.tooltipParams);
-            $scope.offHandItemDetails($scope.heroDetails.items.offHand.tooltipParams);
-            $scope.waistItemDetails($scope.heroDetails.items.waist.tooltipParams)
-            $scope.rightFingerItemDetails($scope.heroDetails.items.rightFinger.tooltipParams)
-            $scope.leftFingerItemDetails($scope.heroDetails.items.leftFinger.tooltipParams)
-            $scope.neckItemDetails($scope.heroDetails.items.neck.tooltipParams)
+            angular.forEach($scope.slots, function (slot) {
+                if ($scope.heroDetails.items[slot]) {
+                    getItemDetails($scope.heroDetails.items[slot].tooltipParams);
+                }
+                else {
+                    $scope.items[slot + "Details"] = '';
+                }
+            });
         }
-        }
-
-     
- )};
-
-   
+    }
+ )
+};
