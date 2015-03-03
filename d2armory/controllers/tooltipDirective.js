8e5188269ee41be964523a209bb71568dde33f91
Novels <codeNovels@gmail.com>
2015-03-03 16:24:53 -0600

    adding files

---
+++ d2armory/controllers/tooltipDirective.js
@@ -0,0 +1,19 @@
+﻿tooltipDirective = angular.module('tooltipDirectives', []);
+
+tooltipDirective.directive('customPopover', function () {
+    return {
+        restrict: 'A',
+        template: '<span>{{label}}</span>',
+        link: function (scope, el, attrs) {
+            scope.label = attrs.popoverLabel;
+            $(el).popover({
+                trigger: 'hover',
+                html: true,
+                content: attrs.popoverHtml,
+                placement: attrs.popoverPlacement
+            });
+        }
+    };
+});
+
+angular.module('tooltipDirective', ['tooltipDirective']);
